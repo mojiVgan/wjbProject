5ff2a3b5cab8096b9300e96c7924ebf4608439ac
MOJI235 <wjbo02@163.com>
2019-03-14 10:43:37 +0800

    添加dateutils维护

---
+++ wjob/src/main/java/utils/DateUtils.java
@@ -0,0 +1,31 @@
+/**
+ * *************************************************************************
+ * * Licensed Material - Property of ShangHai I2finance.                   *
+ * * Copyright (c) ShangHai I2finance Inc. 2009-2017 All rights reserved.  *
+ * * (<http://www.i2finance.net>)                                          *
+ * *
+ * * This file contains proprietary intellectual property of               *
+ * * ShangHai I2finance Inc. The contents of and information in this file  *
+ * * is only to be used in conjunction with a valid I2finance license      *
+ * * as specified in the  I2finance  license agreement. All other use      *
+ * * is prohibited.                                                        *
+ * *************************************************************************
+ */
+
+package utils;
+
+/**
+ Author: wangjiangbo
+ Date: 2019/3/14
+ Time: 10:19
+ */
+public class DateUtils {
+
+
+    /**
+     * 1.创建各种格式string类型的date
+     */
+    /**
+     * 2.比较
+     */
+}
