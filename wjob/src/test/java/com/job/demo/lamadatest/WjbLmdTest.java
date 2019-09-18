af04f741ef580bf3a98a4f6c43d0c34301b72251
MOJI235 <wjbo02@163.com>
2019-09-18 13:39:44 +0800

    lamad相关的demo

--- wjob/src/test/java/com/job/demo/lamadatest/WjbLmdTest.java
+++ wjob/src/test/java/com/job/demo/lamadatest/WjbLmdTest.java
@@ -1,5 +1,6 @@
 package com.job.demo.lamadatest;
 
+import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -25,6 +26,13 @@ public class WjbLmdTest {
 
         System.out.println(strList.stream().collect(Collectors.toSet()));//转成set
         System.out.println(strList);
+        System.out.println("--------------------");
+
+        List<String> collect = strList.stream().map(String::toUpperCase).collect(Collectors.toList());// map的使用
+        List <String> collect1 = strList.stream().map(str -> str.toUpperCase()).collect(Collectors.toList());// ->  和  ::  的使用
+        System.out.println(collect);
+        System.out.println(strList);
+        System.out.println(collect1);
 
         Optional.ofNullable(strList).isPresent();
         System.out.println();
